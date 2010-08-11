873e2c8f1b9c0f8477aedc219a0062e44a140902
Shanny Anoep <s.anoep@gmail.com>
2010-08-11 17:16:29 +0200

    ENH: Refactoring, moved ProcessDialog.

--- src/com/nagazuka/mobile/android/goedkooptanken/PlacesActivity.java
+++ src/com/nagazuka/mobile/android/goedkooptanken/PlacesActivity.java
@@ -1,15 +1,8 @@
 package com.nagazuka.mobile.android.goedkooptanken;
 
-import java.util.Collections;
-import java.util.List;
-
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.app.TabActivity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TabHost;
@@ -17,38 +10,8 @@ import android.widget.TabHost;
 public class PlacesActivity extends TabActivity {
 
 	private static final String TAG = "PlacesActivity";
-	private static final int DIALOG_PROGRESS = 1;
-	private static final int MAX_PROGRESS = 100;
-	private String mPostalCode = "";
 	private String mFuelChoice = "";
-	private ProgressDialog mProgressDialog;
-
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		Dialog dialog = null;
-		switch (id) {
-		case DIALOG_PROGRESS:
-			mProgressDialog = new ProgressDialog(PlacesActivity.this);
-			mProgressDialog.setIcon(R.drawable.ic_gps_satellite);
-			mProgressDialog.setTitle(R.string.progressdialog_title);
-			mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-			mProgressDialog.setMax(MAX_PROGRESS);
-			mProgressDialog.setButton2(getText(R.string.progressdialog_cancel),
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog,
-								int whichButton) {
-
-							/* User clicked No so do some stuff */
-						}
-					});
-			dialog = mProgressDialog;
-		}
-		return dialog;
-	}
 	
-	private void startTabActivities() {
-	}
-
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -59,9 +22,6 @@ public class PlacesActivity extends TabActivity {
 		mFuelChoice = getIntent().getStringExtra(
 				PlacesConstants.INTENT_EXTRA_FUEL_CHOICE);
 
-		new LocationTask().execute();
-		new DownloadTask().execute(mFuelChoice, mPostalCode);
-		
 		// Tab initialization
 		Resources res = getResources(); // Resource object to get Drawables
 		TabHost tabHost = getTabHost(); // The activity TabHost
@@ -70,10 +30,10 @@ public class PlacesActivity extends TabActivity {
 
 		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass(this, PlacesListActivity.class);
-		
-		System.out.println("<< LOG mFuelChoice " + mFuelChoice +" mPostalCode " + mPostalCode +">>");
+
+		Log.d(TAG, "<< mFuelChoice " + mFuelChoice + ">>");
+
 		intent.putExtra(PlacesConstants.INTENT_EXTRA_FUEL_CHOICE, mFuelChoice);
-		intent.putExtra(PlacesConstants.INTENT_EXTRA_POSTAL_CODE, mPostalCode);
 
 		// Initialize a TabSpec for each tab and add it to the TabHost
 		spec = tabHost.newTabSpec("list").setIndicator(
@@ -84,6 +44,7 @@ public class PlacesActivity extends TabActivity {
 
 		// Do the same for the other tabs
 		intent = new Intent().setClass(this, PlacesMapActivity.class);
+		
 		spec = tabHost.newTabSpec("map").setIndicator(
 				res.getString(R.string.map_label),
 				res.getDrawable(R.drawable.ic_tab_map_icons))
@@ -92,57 +53,4 @@ public class PlacesActivity extends TabActivity {
 
 		tabHost.setCurrentTab(0);
 	}
-
-	private class LocationTask extends AsyncTask<Void, Integer, String> {
-		private int mProgress;
-
-		@Override
-		public void onPreExecute() {
-			mProgress = 0;
-			showDialog(DIALOG_PROGRESS);
-			mProgressDialog.setProgress(mProgress);
-
-		}
-
-		@Override
-		protected String doInBackground(Void... params) {
-			for (int i = 1; i <= MAX_PROGRESS; i++) {
-				mProgress++;
-				publishProgress(mProgress);
-			}
-			return "2281 BN";
-		}
-
-		@Override
-		protected void onProgressUpdate(Integer... progress) {
-			mProgressDialog.setProgress(progress[0]);
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			mProgress = MAX_PROGRESS;
-			mProgressDialog.setProgress(mProgress);
-			mProgressDialog.dismiss();
-			
-			mPostalCode = result;
-			
-			Log.d(TAG,"<< mFuelChoice " + mFuelChoice +" mPostalCode " + mPostalCode +">>");
-		}
-
-	}
-
-	private class DownloadTask extends AsyncTask<String, Integer, List<String>> {
-
-		@Override
-		protected List<String> doInBackground(String... params) {
-			List<String> results = Collections.emptyList();
-			return results;
-		}
-		
-		@Override
-		protected void onPostExecute(List<String> result) {
-			startTabActivities();
-		}
-	}
-
 }
--- src/com/nagazuka/mobile/android/goedkooptanken/PlacesListActivity.java
+++ src/com/nagazuka/mobile/android/goedkooptanken/PlacesListActivity.java
@@ -5,39 +5,84 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class PlacesListActivity extends ListActivity {
+	
+	private static final String TAG = "PlacesListActivity";
+	
 	private ListAdapter m_adapter;
+	private ProgressDialog m_progressDialog;
+	private TextView m_headerView;
+
 	private List<Place> m_places = Collections.emptyList();
+	private String m_postalCode = "";
+	private String m_fuelChoice = "";
+	
+	private static final int DIALOG_PROGRESS = 1;
+	private static final int MAX_PROGRESS = 100;
+	private static final String[] GAS_STATIONS = new String[] {
+			"Shell Binckhorst", "BP", "Firezone", "Total", "Elf", "Fina",
+			"Texaco" };
+	private static final String[] ADDRESSES = new String[] {
+			"Caan van Necklaan 118, Rijswijk ZH",
+			"Vsn Campenvaart 49, Den Haag", "Laakweg 214, Den Haag",
+			"Wolmaransstraat 331, Den Haag", "Beechavenue 1, Rozenburg",
+			"Stadhuisplein 1, Rotterdam", "Coolsingel 10, Rotterdam" };
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch (id) {
+		case DIALOG_PROGRESS:
+			m_progressDialog = new ProgressDialog(PlacesListActivity.this);
+			m_progressDialog.setIcon(R.drawable.ic_gps_satellite);
+			m_progressDialog.setTitle(R.string.progressdialog_title);
+			m_progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			m_progressDialog.setMax(MAX_PROGRESS);
+			m_progressDialog.setButton2(getText(R.string.progressdialog_cancel),
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,
+								int whichButton) {
+
+							/* User clicked No so do some stuff */
+						}
+					});
+			dialog = m_progressDialog;
+		}
+		return dialog;
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		String fuelChoice = getIntent().getStringExtra(
+		m_fuelChoice = getIntent().getStringExtra(
 				PlacesConstants.INTENT_EXTRA_FUEL_CHOICE);
-		String postalCode = getIntent().getStringExtra(
-				PlacesConstants.INTENT_EXTRA_POSTAL_CODE);
 
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
 
-		TextView textView = new TextView(getApplicationContext());
-		textView.setText("Resultaten voor " + fuelChoice + " in postcode "
-				+ postalCode + ":");
-		
-		listView.addHeaderView(textView, null, false);
+		m_headerView = new TextView(getApplicationContext());
+		m_headerView.setText("Zoeken naar tankstations voor brandstof " + m_fuelChoice + "...");
+
+		listView.addHeaderView(m_headerView, null, false);
 
 		m_places = getDummyPlaces();
 		m_adapter = new PlacesAdapter(this, R.layout.row, m_places);
 
 		setListAdapter(m_adapter);
-
+		
+		new LocationTask().execute();
 	}
 
 	private static List<Place> getDummyPlaces() {
@@ -50,13 +95,57 @@ public class PlacesListActivity extends ListActivity {
 		return res;
 	}
 
-	private static final String[] GAS_STATIONS = new String[] {
-			"Shell Binckhorst", "BP", "Firezone", "Total", "Elf", "Fina",
-			"Texaco" };
+	private class LocationTask extends AsyncTask<Void, Integer, String> {
+		private int mProgress = 0;
 
-	private static final String[] ADDRESSES = new String[] {
-			"Caan van Necklaan 118, Rijswijk ZH",
-			"Vsn Campenvaart 49, Den Haag", "Laakweg 214, Den Haag",
-			"Wolmaransstraat 331, Den Haag", "Beechavenue 1, Rozenburg",
-			"Stadhuisplein 1, Rotterdam", "Coolsingel 10, Rotterdam" };
+		@Override
+		public void onPreExecute() {
+			showDialog(DIALOG_PROGRESS);
+			m_progressDialog.setProgress(mProgress);
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			for (int i = 1; i <= MAX_PROGRESS; i++) {
+				mProgress++;
+				publishProgress(mProgress);
+			}
+			return "2281 BN";
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			m_progressDialog.setProgress(progress[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			mProgress = MAX_PROGRESS;
+			m_progressDialog.setProgress(mProgress);
+			m_progressDialog.dismiss();
+
+			m_postalCode = result;
+
+			Log.d(TAG, "<< LocationTask: mFuelChoice " + m_fuelChoice + " m_postalCode "
+					+ m_postalCode + ">>");
+			m_headerView.setText("Locatie gevonden, postcode: " + m_postalCode);
+			new DownloadTask().execute(m_fuelChoice, m_postalCode);
+		}
+	}
+
+	private class DownloadTask extends AsyncTask<String, Integer, List<Place>> {
+
+		@Override
+		protected List<Place> doInBackground(String... params) {
+			List<Place> results = getDummyPlaces();
+			return results;
+		}
+
+		@Override
+		protected void onPostExecute(List<Place> result) {
+			Log.d(TAG, "<< DownloadTask: result size = "+ result.size() + ">>");
+			//m_places.addAll(result);
+		}
+	}
+
 }
