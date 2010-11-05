7af4ee84d5a0a99434403b6b3b30bcd1dd94a429
Shanny Anoep <s.anoep@gmail.com>
2010-11-05 13:11:07 +0100

    ENH: Untested first draft of GoogleHTTPGeocodingService

--- src/com/nagazuka/mobile/android/goedkooptanken/service/impl/GoogleHttpGeocodingService.java
+++ src/com/nagazuka/mobile/android/goedkooptanken/service/impl/GoogleHttpGeocodingService.java
@@ -1,8 +1,6 @@
 package com.nagazuka.mobile.android.goedkooptanken.service.impl;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -10,22 +8,14 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 import com.nagazuka.mobile.android.goedkooptanken.exception.GoedkoopTankenException;
 import com.nagazuka.mobile.android.goedkooptanken.exception.NetworkException;
-import com.nagazuka.mobile.android.goedkooptanken.model.AddressComponent;
-import com.nagazuka.mobile.android.goedkooptanken.model.AddressComponentType;
-import com.nagazuka.mobile.android.goedkooptanken.model.GeocodingResult;
-import com.nagazuka.mobile.android.goedkooptanken.model.LocationType;
 import com.nagazuka.mobile.android.goedkooptanken.model.Place;
 import com.nagazuka.mobile.android.goedkooptanken.service.GeocodingService;
 
@@ -38,21 +28,22 @@ public abstract class GoogleHttpGeocodingService implements GeocodingService {
 	public String getPostalCode(double latitude, double longitude)
 			throws GoedkoopTankenException {
 		String postalCode = "";
-		
+
 		String url = constructReverseGeocodingURL(latitude, longitude);
 		String jsonResponse = download(url);
 		postalCode = parsePostalCode(jsonResponse);
-		
+
 		return postalCode;
 	}
-	
-	private static String constructReverseGeocodingURL(double latitude, double longitude) {
+
+	private static String constructReverseGeocodingURL(double latitude,
+			double longitude) {
 		String URL = URL_GOOGLE_API;
-		//TODO: region?
-		String params = "json?latlng=" + latitude + "," + longitude + "&sensor=true";
+		// TODO: region?
+		String params = "json?latlng=" + latitude + "," + longitude
+				+ "&sensor=true";
 		return URL + params;
 	}
-	
 
 	@Override
 	public double[] getLocation(Place place) throws GoedkoopTankenException {
@@ -94,51 +85,93 @@ public abstract class GoogleHttpGeocodingService implements GeocodingService {
 
 		return response;
 	}
-	
+
 	private String parsePostalCode(String response) {
 		String postalCode = "";
 		try {
-			GeocodingResult geoResult = unmarshall(response);
-		}
-		catch (Exception e) {
+			JSONObject jsonResponse = new JSONObject(response);
+			if (jsonResponse.has("status")) {
+				String status = jsonResponse.getString("status");
+				if (!status.equals("OK")) {
+					Log.e(TAG,
+							"<< Exception occurred for geocoding, HTTP response: "
+									+ response);
+					throw new GoedkoopTankenException(
+							"Uw postcode kan niet bepaald worden. Onbekende fout opgetreden.",
+							null);
+				}
+			}
+			JSONArray results = getResultsArray(jsonResponse);
+			postalCode = getPostalCode(results);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
 		return postalCode;
 	}
-	
-	protected GeocodingResult unmarshall(String jsonString) {
-		Gson gson = getGsonBuilder().create();
-		return gson.fromJson(jsonString, GeocodingResult.class);
-	}
-	
-	/**
-	 * Gets the gson builder.
-	 * 
-	 * @return the gson builder
-	 */
-	protected GsonBuilder getGsonBuilder() {
-		GsonBuilder builder = new GsonBuilder();
-		builder.setDateFormat(RFC822DATEFORMAT);
-		builder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
-		builder.registerTypeAdapter(LocationType.class, new JsonDeserializer<LocationType>() {
-			@Override
-			public LocationType deserialize(JsonElement arg0, Type arg1,
-					JsonDeserializationContext arg2) throws JsonParseException {
-				return LocationType.fromValue(arg0.getAsString());
-			}
-		});
-		builder.registerTypeAdapter(AddressComponentType.class, new JsonDeserializer<AddressComponentType>() {
-			@Override
-			public AddressComponentType deserialize(JsonElement arg0, Type arg1,
-					JsonDeserializationContext arg2) throws JsonParseException {
-				return AddressComponentType.fromValue(arg0.getAsString());
-			}
-		});
-		
-		return builder;
+
+	private String getPostalCode(JSONArray results) throws JSONException {
+		for (int i = 0; i < results.length(); i++) {
+			JSONObject result = (JSONObject) results.get(i);
+			JSONArray addrComps = getAddressComponents(result);
+			getPostalCodeFromAddressComponents(addrComps);
+		}
+		return null;
 	}
 
-    /** The Constant RFC822DATEFORMAT. */
-    public static final String RFC822DATEFORMAT = "EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z";
+	private JSONArray getAddressComponents(JSONObject result)
+			throws JSONException {
+		JSONArray addrComps = null;
+		if (result.has("address_components")) {
+			addrComps = result.getJSONArray("address_components");
+		}
+		return addrComps;
+	}
 
+	private String getPostalCodeFromAddressComponents(
+			JSONArray addressComponents) throws JSONException {
+		String postalCode = null;
+		for (int i = 0; i < addressComponents.length(); i++) {
+			JSONObject addrComp = (JSONObject) addressComponents.get(i);
+			if (addrComp.has("types")) {
+				JSONArray types = addrComp.getJSONArray("types");
+				if (hasPostalCodeType(types)) {
+					postalCode = getPostalCodeFromAddressComponent(addrComp);
+					break;
+				}
+			}
+		}
+		return postalCode;
+	}
+
+	private String getPostalCodeFromAddressComponent(JSONObject addrComp)
+			throws JSONException {
+		String postalCode = null;
+		if (addrComp.has("long_name")) {
+			postalCode = addrComp.getString("long_name");
+		} else if (addrComp.has("short_name")) {
+			postalCode = addrComp.getString("short_name");
+		}
+		return postalCode;
+	}
+
+	private boolean hasPostalCodeType(JSONArray types) throws JSONException {
+		boolean res = false;
+		for (int i = 0; i < types.length(); i++) {
+			String type = (String) types.get(i);
+			if (type.equals("postal_code")) {
+				res = true;
+			}
+
+		}
+		return res;
+	}
+
+	private JSONArray getResultsArray(JSONObject jsonResponse)
+			throws JSONException {
+		JSONArray results = null;
+		if (jsonResponse.has("results")) {
+			results = jsonResponse.getJSONArray("results");
+		}
+		return results;
+	}
 }
