4b7a12b8b4448f64e1748b0ccbaa143824c7c448
Shanny Anoep <s.anoep@gmail.com>
2010-11-24 21:28:23 +0100

    ENH: Added Util class

---
+++ src/com/nagazuka/mobile/android/goedkooptanken/util/PlacesUtil.java
@@ -0,0 +1,38 @@
+package com.nagazuka.mobile.android.goedkooptanken.util;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.nagazuka.mobile.android.goedkooptanken.model.Place;
+
+public class PlacesUtil {
+	private static final String TAG = PlacesUtil.class.getName();
+
+	public static Intent getGoogleMapsIntent(Place selectedItem) {
+		Uri geoUri = createGeoURI(selectedItem, false);
+		Intent mapCall = new Intent(Intent.ACTION_VIEW, geoUri);
+		return mapCall;
+	}
+
+	public static Intent getGoogleNavigationIntent(Place selectedItem) {
+		Uri geoUri = createGeoURI(selectedItem, true);
+		Intent mapCall = new Intent(Intent.ACTION_VIEW, geoUri);
+		return mapCall;
+	}
+
+	public static Uri createGeoURI(Place selectedItem, boolean navigation) {
+		String geoUriString;
+		if (!navigation) {
+			geoUriString = "geo:0,0?q=Nederland, ";
+		} else {
+			geoUriString = "google.navigation:q=Nederland, ";
+		}
+
+		geoUriString += selectedItem.getAddress() + ", "
+				+ selectedItem.getPostalCode() + "," + selectedItem.getTown();
+
+		Uri geoUri = Uri.parse(geoUriString);
+		return geoUri;
+	}
+}
